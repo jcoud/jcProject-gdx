9e7e0f3205e788d9def95d8242bd92eb1c52143a
jcoud_ <jhcoud@gmail.com>
2019-02-19 01:09:04 +0500

    Initial commit

---
+++ desktop/src/jcp/game/data/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package jcp.game.data.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import jcp.game.data.the_game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new the_game(), config);
+	}
+}
